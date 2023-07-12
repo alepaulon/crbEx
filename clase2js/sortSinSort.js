81adbef703647e13d0176251ad4e7f8ddfc88c03
Alejo Paulon <alejo.paulon@gmail.com>
2023-07-12 18:24:00 -0300

    ej 2da clase terminados

--- clase2js/sortSinSort.js
+++ clase2js/sortSinSort.js
@@ -1,2 +1,14 @@
 // Ordenar el siguiente array sin usar sort: [3, 23, 11, 15, 6, 19, 32, 9]
 
+let arr = [3, 23, 11, 15, 6, 19, 32, 9];
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  console.log("Array ordenado:", arr);
